cea5e7822b7e9842356e7fd1e0add8e0fbd9d6cf
fillipe.santos <fillipe.santos@GMAIL.COM>
2022-10-11 16:58:34 -0300

    OK

---
+++ projetoBuscaAltoVale_Senac-FILLIPE-SANTOS/sistemabuscaaltovaleok/src/lib/RegraDeNegócio.java
@@ -0,0 +1,114 @@
+//
+//    #Regra de negócios:
+//
+//    Tela principal
+//        Telas de login (juridico e fisico)
+//            login fisico 
+//                CPF ou Email e senha
+//                Botão de cadastrar a coonta
+//                Botao de entrar (ir para o painel de clientes)
+//                Botão de sair
+//
+//            login juridico
+//                CNPJ ou Email e senha
+//                Botão de ir cadastrar a conta
+//                Botao de entrar (ir para painel de empresa)
+//                Botao de sair 
+//
+//
+ //      Telas de cadastro (juridico e fisico)
+//            Cadastro fisico
+//                Nome completo ok
+//                cpf ok
+//                Localização ok
+//                    Cidade ok
+//                    Estado ok
+//                email ok 
+//                senha ok
+//                Cancelar ok 
+//                Confirmarok
+//                Ir para tela de login fisico OK
+//
+//            Cadastro juridico
+//                Razão Social
+//                CNPJ
+//                Localização
+//                    Cidade
+//                    Estado
+//                email
+//                senha
+//                descrição
+//                area de atuação
+//                Cancelar
+//                Confirmar
+//                Ir para tela de login juridico
+//
+//          
+//
+//
+//
+//        Tela de Administrador
+//            login de adm (leva para o painel de adm)
+//
+//        Sair (Fechar o sistema)
+//
+//       
+//
+//
+//
+//
+//
+//
+//
+//
+//    Tela de painel de Cliente 
+//        Consultar as empresas ok
+//            Razão Social ok 
+//            E-mail
+//            Telefone
+//            Área de atuação ok
+    //          vagas disponíveis ok
+//            
+//        Alterar o cadastro ok
+//        
+//        Excluir o cadastro ok
+//            
+//       vagas cadastradas ok
+//            
+//        Sair do sistema ok
+//        ir para login ok
+//
+//    Tela de painel de empresas
+//        Consultar pessoas que se inscreveram
+//            Nome Completo
+//            Email
+//            Telefone
+//
+//        Sair do sistema
+//
+//        Alterar o cadastro
+//
+//        Excluir o cadastro
+//
+//
+//    Tela de administrador
+//        Consultar todos os dados de empresas e clientes
+//        Alterar cadastro de empresa ou cliente pelo codigo
+//        Excluir cadastro de empresa ou cliente pelo codigo
+//        Adicionar cadastro de empresa ou cliente pelo codigo
+//
+//    Sair
+//        Sai do sistema
+//
+//
+//
+//
+//
+//        #Banco de dados
+//
+//     
+//
+//        
+//    
+//
+//    
---
+++ projetoBuscaAltoVale_Senac-FILLIPE-SANTOS/sistemabuscaaltovaleok/src/view/PainelLoginFisico.java
@@ -0,0 +1,182 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package view;
+
+/**
+ *
+ * @author Usuario
+ */
+public class PainelLoginFisico extends javax.swing.JFrame {
+    
+    PainelCliente plFisico = new PainelCliente();
+
+    /**
+     * Creates new form PainelLoginFísico
+     */
+    public PainelLoginFisico() {
+        initComponents();
+        this.setLocationRelativeTo(null);
+    }
+
+   
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jComboBox1 = new javax.swing.JComboBox<>();
+        buttonGroup1 = new javax.swing.ButtonGroup();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        edtCpfOuEmail = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        edtSenhaFisico = new javax.swing.JPasswordField();
+        btnSair = new javax.swing.JButton();
+        BtnEntrar = new javax.swing.JButton();
+
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(0, 0, 204));
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("LOGIN");
+
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel2.setText("CPF ou E-mail:");
+
+        edtCpfOuEmail.setHorizontalAlignment(javax.swing.JTextField.LEFT);
+        edtCpfOuEmail.setToolTipText("");
+        edtCpfOuEmail.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                edtCpfOuEmailActionPerformed(evt);
+            }
+        });
+
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel3.setText("Senha:");
+
+        edtSenhaFisico.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                edtSenhaFisicoActionPerformed(evt);
+            }
+        });
+
+        btnSair.setText("Sair");
+        btnSair.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSairActionPerformed(evt);
+            }
+        });
+
+        BtnEntrar.setText("Entrar");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 84, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(169, 169, 169))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(144, 144, 144)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel2)
+                    .addComponent(edtCpfOuEmail, javax.swing.GroupLayout.DEFAULT_SIZE, 136, Short.MAX_VALUE)
+                    .addComponent(edtSenhaFisico, javax.swing.GroupLayout.DEFAULT_SIZE, 136, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(btnSair, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(BtnEntrar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addGap(144, 144, 144))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(36, 36, 36)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(edtCpfOuEmail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(edtSenhaFisico, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnSair)
+                    .addComponent(BtnEntrar))
+                .addContainerGap(82, Short.MAX_VALUE))
+        );
+
+        edtCpfOuEmail.getAccessibleContext().setAccessibleName("");
+        edtCpfOuEmail.getAccessibleContext().setAccessibleParent(jLabel1);
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void edtCpfOuEmailActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_edtCpfOuEmailActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_edtCpfOuEmailActionPerformed
+
+    private void edtSenhaFisicoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_edtSenhaFisicoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_edtSenhaFisicoActionPerformed
+
+    private void btnSairActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSairActionPerformed
+        plFisico.setVisible(true);
+    }//GEN-LAST:event_btnSairActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(PainelLoginFisico.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(PainelLoginFisico.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(PainelLoginFisico.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(PainelLoginFisico.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new PainelLoginFisico().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton BtnEntrar;
+    private javax.swing.JButton btnSair;
+    private javax.swing.ButtonGroup buttonGroup1;
+    private javax.swing.JTextField edtCpfOuEmail;
+    private javax.swing.JPasswordField edtSenhaFisico;
+    private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    // End of variables declaration//GEN-END:variables
+}
